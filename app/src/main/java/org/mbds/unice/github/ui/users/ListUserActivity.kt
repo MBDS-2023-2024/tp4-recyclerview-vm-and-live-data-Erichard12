0c362e0c6e6e4fa1b516a206cdc3e61508cfaf2d
emman <emmanuelrichard1290@gmail.com>
2024-09-05 16:56:22 -0400

    add view binding for listViewActivity

--- app/src/main/java/org/mbds/unice/github/ui/users/ListUserActivity.kt
+++ app/src/main/java/org/mbds/unice/github/ui/users/ListUserActivity.kt
@@ -1,17 +1,23 @@
 package org.mbds.unice.github.ui.users
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.mbds.unice.github.R
 import org.mbds.unice.github.data.model.User
+import org.mbds.unice.github.databinding.ActivityListUserBinding
 
 class ListUserActivity : AppCompatActivity(), UserListAdapter.Listener {
     // TODO : Utiliser viewBinding
     lateinit var recyclerView: RecyclerView
     lateinit var fab: FloatingActionButton
+    lateinit var binding: ActivityListUserBinding
+
 
     // By lazy permet de faire du chargement parresseux,
     // L'adapteur sera crée au premier appel
@@ -25,7 +31,10 @@ class ListUserActivity : AppCompatActivity(), UserListAdapter.Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_user)
+        //  Permet de lier automatiquement les vues entre le XML et la vue
+        binding = ActivityListUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         configureFab()
         configureRecyclerView()
     }
@@ -38,19 +47,44 @@ class ListUserActivity : AppCompatActivity(), UserListAdapter.Listener {
     }
 
     private fun configureRecyclerView() {
-        recyclerView = findViewById(R.id.activity_list_user_rv)
-        recyclerView.adapter = adapter
+        // recyclerView = findViewById(R.id.activity_list_user_rv)
+        binding.activityListUserRv.adapter = adapter
+        //recyclerView.adapter = adapter
     }
 
     private fun configureFab() {
-        fab = findViewById(R.id.activity_list_user_fab)
-        fab.setOnClickListener {
-            TODO("Ajouter un utilisateur aléatoire")
+        //fab = findViewById(R.id.activity_list_user_fab)
+        binding.activityListUserFab.setOnClickListener {
+            viewModel.generateRandomUser()
+            Toast.makeText(this, "adding user", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun onClickDelete(user: User) {
-        TODO("Ajouter des logs pour tracer les actions de l'utilisateur")
-        TODO("Ajouter une boite de dialogue pour confirmer la suppression et supprimer l'utilisateur si l'utilisateur confirme")
+        //Ajouter des logs pour tracer les actions de l'utilisateur
+        Log.d("UserAction", "Delete requested for user: $user")
+        //Ajouter une boite de dialogue pour confirmer la suppression et supprimer l'utilisateur si l'utilisateur confirme"
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Confirm delete")
+        builder.setMessage("Are you sure you want to delete $user?")
+
+        // If the user confirms deletion
+        builder.setPositiveButton("yes") { dialog, _ ->
+            // Perform the deletion here
+            viewModel.deleteUser(user)
+            Log.d("UserAction", "User $user deleted")
+            dialog.dismiss() // Close the dialog
+
+            // If the user cancels deletion
+            builder.setNegativeButton("NO") { dialog, _ ->
+                Log.d("UserAction", "Deletion canceled for user: ${user}")
+                dialog.dismiss() // Close the dialog
+            }
+
+            // show the dialog
+            val dialog = builder.create()
+            dialog.show()
+
+        }
     }
 }
